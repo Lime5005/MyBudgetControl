173d363fd1dbe1325a577b989e0d10ce3a94889e
Lime5005 <zlp5005@gmail.com>
2020-12-21 02:49:13 +0100

    Initial Commit

---
+++ BudgetControl/BudgetControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetControlApp.swift
+//  BudgetControl
+//
+//  Created by Liping Mechling on 21/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct BudgetControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
