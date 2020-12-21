a379bdeba2a212b473b18d7ca1ad0c0e7272c72b
Lime5005 <zlp5005@gmail.com>
2020-12-21 03:22:53 +0100

    add the basic function

--- BudgetControl/ContentView.swift
+++ BudgetControl/ContentView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var totalBudegt = ""
+    @State private var savingGoal = ""
+    @State private var usedToday = ""
+    
+    //computed property
+    var totalLeft: Double {
+        let budgetAmount = Double(totalBudegt) ?? 0
+        let controlGoal = Double(savingGoal) ?? 0
+        let howMuchUsed = Double(usedToday) ?? 0
+        
+        let totalLeft = budgetAmount - controlGoal - howMuchUsed
+        
+        return totalLeft
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("Total budget:")) {
+                    TextField("Enter your total budget for this month", text: $totalBudegt)
+                        .keyboardType(.decimalPad)
+                }
+                Section(header: Text("Saving goal:")) {
+                    TextField("Enter your saving goal for this month",
+                              text: $savingGoal)
+                        .keyboardType(.decimalPad)
+                    
+                }
+                
+                Section(header: Text("How much I just used:")) {
+                    TextField("Enter the amount just used",
+                              text: $usedToday)
+                        .keyboardType(.decimalPad)
+                    
+                }
+                
+                //How to remember the last time input?
+                Section(header: Text("How much left:")) {
+                    Text("\(totalLeft, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("My Budget Control")
+        }
     }
 }
 
